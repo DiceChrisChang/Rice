d68cbefd336dc0c25f0a340ca663751f1f64c9e1
Dice <ChrisChangDice@163.com>
2017-12-07 16:00:40 +0800

    backgroundImage

--- application/views/Firstenter/head.php
+++ application/views/Firstenter/head.php
@@ -15,110 +15,12 @@
 	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<!-- 
 		自行引入的 CSS 文件
-		尚未成功引入
+		成功引入
 	-->
-	<link rel="stylesheet" type="text/css" href="application/views/Firstenter/firstenter.css">
-	<!-- 暂时添加的 css -->
-	<style type="text/css">
-        html{
-            width:100%;
-            overflow-x:hidden;
-        }
-        body{
-            margin-bottom: 50px;
-        }
-        .foot_p{
-        	color:white;
-        	text-align: center;
-        }
-        .img-circle{
-        	border-radius: 20%;
-        	margin-top: 60px;
-        }
-        .jumbotron h1{
-            margin-top: 170px;
-        }
-        .jumbotron p{
-        	margin-top: 40px;
-        	font-size: 25px;
-        }
-        #loginButton{
-        	margin-top: 130px;
-            margin-bottom: 80px;
-        }
-        .jumbotron{
-            padding-bottom: 20px; 
-            background-image: url(img/2012.jpg);
-            /* img url 需设置通用路径 仅有登录页面使用 使用 js */
-            background-size: cover;
-            background-repeat:no-repeat;
-            overflow:hidden;
-            width:100%;
-            min-height: 692px;
-            overflow: hidden;
-            margin-top:51px;
-            background-size:100% auto;
-            margin-bottom: 0px;
-           
-        } 
-        .container-fluid{
-            width:100%;
-            height:100%;
-        }
-        .btn{
-        	margin-right: 6px;
-        	position:relative;
-        	top:6px;
-        }
-        .btn-lg{
-        	position:relative;
-        	top:0px;        	
-        }
-        .foot_p{
-        	margin-top: 10px;
-        }
+	<link rel="stylesheet" type="text/css" href="css/firstenter.css">
+    <link rel="stylesheet" type="text/css" href="css/modal.css">
+    <link rel="stylesheet" type="text/css" href="css/notice.css">
+    <link rel="stylesheet" type="text/css" href="css/path-navigation.css">
 
-        /*
-        modal
-        */
-        .modal-header{
-            padding: 0px 0px 0px 0px;
-            margin: 0px  0px 0px 0px;
-        }
 
-        /*
-        path-navigation
-        */
-        .breadcrumb{
-            margin-bottom: 0px;
-        }
-
-    
-        /*
-        notice css
-        */
-        .img-rounded{
-            float: left;
-            width: 470px;
-            height: 650px;
-        }
-        #navbar-notice{
-            width: 1100px;
-            height: auto;
-            float: right;
-            position: relative;
-            left: 0px;
-            overflow: auto;
-        }
-        .bg-primary,.bg-info,.bg-success,.bg-warning{
-            padding: 8px 3px 8px 3px;
-            margin: 0px  5px 5px 4px;
-        }
-        #ios,#svn,#jmeter{
-            visibility:hidden;
-            margin :0px 0px 0px 0px;
-            height: 2px;
-        }
-
-    </style>
 </head>
