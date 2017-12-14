7e83de4c49795b2a56a9bb28079450d4617ee472
Dice <ChrisChangDice@163.com>
2017-12-14 17:23:23 +0800

    template modal 2

--- application/views/Inventory/modal.php
+++ application/views/Inventory/modal.php
@@ -1,6 +1,8 @@
     <!-- 
     模态框（Modal）
     模态框不能放进 nav-bar
+    id:myModal 
+    不可以修改
      -->
   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -58,7 +60,7 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="myModalLabel">Modal title</h4>
       </div>
-      <div class="modal-body">
+      <div class="modal-body"> 
         ...
       </div>
       <div class="modal-footer">
@@ -68,3 +70,62 @@
     </div>
   </div>
 </div>
+
+
+
+
+<!-- -->
+<!-- tenplate modal all kinds -->
+<div class="modal fade" id=" " tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- head -->
+            <div class="modal-header bg-primary">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title " id="myModalLabel">  template  </h4>
+            </div>
+            <!-- body -->
+            <div class="modal-body"> 
+               <!-- form 表单 -->
+              <form class="form-horizontal" method="post" action="login">
+                <!-- 带计量单位的输入 -->
+                <div class="input-group">
+                    <span class="input-group-addon">质量</span>
+                    <input type="text" class="form-control">
+                    <span class="input-group-addon">kg</span>
+                </div>
+                <!-- 普通输入框 -->
+                <div class="form-group">
+                  <label for="exampleInputName2" >Name</label>
+                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+                </div>
+                <!-- checkbox choose -->
+                <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox"> Remember me
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <!-- 下拉选择项 -->
+                <label for="cool">产品号</label>
+                <select class="form-control" id="cool">
+                    <option>201701</option>
+                    <option>201702</option>
+                    <option>201703</option>
+                    <option>201704</option>
+                    <option>201705</option> 
+                </select>
+              </form><!-- form close -->
+            </div>
+            <!-- footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                <button type="submit" class="btn btn-primary">登录</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
--- application/views/Inventory/operate.php
+++ application/views/Inventory/operate.php
@@ -4,7 +4,7 @@
         <button type="button" class="btn btn-primary" aria-label="Left Align" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
         <span class="glyphicon glyphicon-list" aria-hidden="true"></span>
         </button>
-          
+
           <!-- 按钮触发模态框 -->
           <!-- Button trigger modal -->
           <button type="button" class="btn btn-primary " data-toggle="modal" data-target="#Modal">
@@ -16,7 +16,7 @@
 
 
 
-
+ 
 
 
 <div class="collapse" id="collapseExample">
@@ -34,16 +34,21 @@
           </select>
           </div>
             <!-- 组合筛选 -->
+
           <div class="row">
              <div class="form-group">
-               <label for="exampleInputName2">Name</label>
-               <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+               <label for="exampleInputName2" >Name</label>
+                 <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
              </div>
              <div class="form-group">
                <label for="exampleInputEmail2">Email</label>
-               <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
              </div>
           </div>
+
+
+
+
           <div class="row">
              <button type="submit" class="btn btn-primary">筛选 </button>
              <button type="button" class="btn btn-primary" data-target="#">全选</button>
