3c339b9c1639f0d1b92aab0139445020f9650719
Dice <ChrisChangDice@163.com>
2018-01-06 16:01:01 +0800

    add sql

--- SQL/manager_up.sql
+++ SQL/manager_up.sql
@@ -21,3 +21,54 @@
 -- ( 3, 1, '[0],[1]', '仓储部', '仓储部', '', NULL),
 -- ( 4, 1, '[0],[1]', '管理部', '管理部', '', NULL);
 
+-- 创建 role table 
+-- DROP TABLE IF EXISTS `role`;
+-- CREATE TABLE IF NOT EXISTS `role` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+--   `num` int(11) DEFAULT NULL COMMENT '序号',
+--   `pid` int(11) DEFAULT NULL COMMENT '父角色id',
+--   `name` varchar(255) DEFAULT NULL COMMENT '角色名称',
+--   `deptid` int(11) DEFAULT NULL COMMENT '部门名称',
+--   `tips` varchar(255) DEFAULT NULL COMMENT '提示',
+--   `version` int(11) DEFAULT NULL COMMENT '保留字段(暂时没用）',
+--   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '角色创建时间',
+--   `changed_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '角色修改时间',
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+ -- 向 role table 插入数据
+ -- INSERT INTO `role` (`id`, `num`, `pid`, `name`, `deptid`, `tips`, `version`) VALUES
+ -- ( 1, 1, 0, '超级管理员', 1, '全局超级管理员', NULL),
+ -- ( 2, 2, 1, '订单部管理员', 2, '订单管理', NULL),
+ -- ( 3, 3, 2, '订单部操作员', 2, '订单管理', NULL),
+ -- ( 4, 4, 1, '仓储部管理员', 3, '仓储管理', NULL),
+ -- ( 5, 5, 4, '仓储部操作员', 3, '仓储管理', NULL),
+ -- ( 6, 6, 1, '管理部管理员', 4, '统计监察管理', NULL);
+
+
+ 创建 user table
+ DROP TABLE IF EXISTS `user`;
+ CREATE TABLE IF NOT EXISTS `user` (
+   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+   `name` varchar(45) DEFAULT NULL COMMENT '名字',
+   `account` varchar(45) DEFAULT NULL COMMENT '账号',
+   `password` varchar(45) DEFAULT NULL COMMENT '密码',
+   `salt` varchar(255) DEFAULT NULL COMMENT 'md5密码盐',
+   `email` varchar(45) DEFAULT NULL COMMENT '电子邮件',
+   `phone` varchar(45) DEFAULT NULL COMMENT '电话',
+   `roleid` int(11) DEFAULT NULL COMMENT '角色id',
+   `deptid` int(11) DEFAULT NULL COMMENT '部门id',
+   `status` int(11) DEFAULT NULL COMMENT '状态(1：启用  2：冻结 ）',
+   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '用户创建时间',
+   `changed_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户修改时间',
+   `version` int(11) DEFAULT NULL COMMENT '保留字段',
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
+
+
+
+INSERT INTO `user` (`name`, `account`, `password`, `salt`, `email`, `phone`, `roleid`, `deptid`, `status`, `create_time`,`changed_time`) VALUES
+('李方彦'，'admin',)
+
+(1, 'girl.gif', 'admin', 'ecfadcde9305f8891bcfe5a1e28c253e', '8pgby', '张三', '2017-05-05 00:00:00', 2, 'sn93@qq.com', '18200000000', '1', 27, 1, '2016-01-29 08:49:53', 25),
+(44, NULL, 'test', '45abb7879f6a8268f1ef600e6038ac73', 'ssts3', 'test', '2017-05-01 00:00:00', 1, 'abc@123.com', '', '5', 26, 1, '2017-05-16 20:33:37', NULL);
