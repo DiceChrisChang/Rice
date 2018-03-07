00d69841cc1293225a4df27bfaa23261df54bfed
Dice <ChrisChangDice@163.com>
2018-03-07 15:01:24 +0800

    Q&A

--- application/views/head.php
+++ application/views/head.php
@@ -5,17 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    
-	<title><?php echo $this->lang->line('system_system_name'); ?></title>
+
+	<title><?php echo $this->lang->line('system_system_name'); //按照书添加 ?></title>
 	<!-- Bootstrap core CSS -->
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
 	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 	<!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
 	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
     <!-- 引入 ECharts 文件 -->
     <script src="/rice/echarts.js"></script>
+
 	<!-- 
 		自行引入的 CSS 文件
 		成功引入
@@ -30,7 +31,6 @@
     <style type="text/css">
     	/*
     	备用css
-
         finally delete
     	*/
     	html{
@@ -161,3 +161,4 @@
 
 
 </head>
+<!-- end head.php -->
