b053b2f257fc6e2a934cefe6bd34639fd52620f9
Dice <ChrisChangDice@163.com>
2018-03-07 15:45:57 +0800

    error

---
+++ application/language/english/en_admin_lang.php
@@ -0,0 +1,8 @@
+// Common form elements
+$lang['common_form_elements_next'] = "Next...";
+$lang['common_form_elements_save'] = "Save...";
+$lang['common_form_elements_cancel'] = "Cancel...";
+$lang['common_form_elements_go'] = "Go...";
+$lang['common_form_elements_go_back'] = "Go back";
+$lang['common_form_elements_submission_error'] = "There were errors";
+$lang['common_form_elements_next'] = "Next...";
