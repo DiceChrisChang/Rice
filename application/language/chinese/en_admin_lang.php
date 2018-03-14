82e680a514ad443fef4bf52a0339217d7b67aeed
Dice <ChrisChangDice@163.com>
2018-03-14 13:35:08 +0800

    language_notice

---
+++ application/language/chinese/en_admin_lang.php
@@ -0,0 +1,27 @@
+// Common form elements
+$lang['common_form_elements_next'] = "继续...";
+$lang['common_form_elements_save'] = "保存...";
+$lang['common_form_elements_cancel'] = "取消...";
+<!--
+	$lang['common_form_elements_go'] = "操作执行";
+-->
+$lang['common_form_elements_go'] = "Go...";
+$lang['common_form_elements_go_back'] = "返回";
+$lang['common_form_elements_submission_error'] = "提交表单形式存在错误:";
+$lang['common_form_elements_success_notifty'] = "成功提示:";
+$lang['common_form_elements_error_notifty'] = "错误提示:";
+$lang['common_form_elements_actions'] =  "操作提示";
+$lang['common_form_elements_actions_edit'] =  "操作修改";
+$lang['common_form_elements_actions_delete'] =  "操作删除";
+$lang['common_form_elements_active'] =  "激活状态";
+$lang['common_form_elements_inactive'] =  "未激活状态";
+$lang['common_form_elements_successful_change'] = "您的修改操作已经被保存";
+$lang['common_form_elements_successful_delete'] = "您的删除操作已经成功";
+$lang['common_form_elements_yes'] = "是";
+$lang['common_form_elements_no'] = "否";
+$lang['common_form_elements_to'] = "to";
+$lang['common_form_elements_from'] = "from";
+$lang['common_form_elements_history'] = "历史记录";
+
+
+
--- application/language/english/en_admin_lang.php
+++
@@ -1,8 +0,0 @@
-// Common form elements
-$lang['common_form_elements_next'] = "Next...";
-$lang['common_form_elements_save'] = "Save...";
-$lang['common_form_elements_cancel'] = "Cancel...";
-$lang['common_form_elements_go'] = "Go...";
-$lang['common_form_elements_go_back'] = "Go back";
-$lang['common_form_elements_submission_error'] = "There were errors";
-$lang['common_form_elements_next'] = "Next...";
